2f2acd4ed9e68475b20b65daef8f00e3b1a9d4dc
Oleksandr <akm.oleksandr@gmail.com>
2023-12-16 23:54:16 -0600

    inference for LLM created

--- inference.py
+++ inference.py
@@ -6,74 +6,50 @@ from datasets import load_dataset
 from dataset import LLMDataset
 import torch
 
-def get_translation(config, sentence: str):
+@torch.no_grad()
+def get_response(config, text):
     # Define the device, tokenizers, and model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("Using device:", device)
-    tokenizer_src = Tokenizer.from_file(str(Path(config['tokenizer_file'].format(config['lang_src']))))
-    tokenizer_trgt = Tokenizer.from_file(str(Path(config['tokenizer_file'].format(config['lang_trgt']))))
-    model = build_transformer(tokenizer_src.get_vocab_size(), tokenizer_trgt.get_vocab_size(), config["seq_len"], config['seq_len'], d_model=config['d_model']).to(device)
-
+    tokenizer = Tokenizer.from_file(str(Path(config['tokenizer_file'])))
+    model = build_transformer(tokenizer.get_vocab_size(), config["seq_len"], config['d_model']).to(device)
+    
     # Load the pretrained weights
     model_filename = latest_weights_file_path(config)
     state = torch.load(model_filename)
     model.load_state_dict(state['model_state_dict'])
 
-    # if the sentence is a number use it as an index to the test set
-    label = ""
-    if type(sentence) == int or sentence.isdigit():
-        id = int(sentence)
-        ds = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_trgt']}", split='all')
-        ds = LLMDataset(ds, tokenizer_src, tokenizer_trgt, config['lang_src'], config['lang_trgt'], config['seq_len'])
-        sentence = ds[id]['src_text']
-        label = ds[id]["trgt_text"]
-    seq_len = config['seq_len']
-
-    # Initialize an empty string to store the translation
-    translation_result = ""
-    # translate the sentence
     model.eval()
-    with torch.no_grad():
-        # Precompute the encoder output and reuse it for every generation step
-        source = tokenizer_src.encode(sentence)
-        source = torch.cat([
-            torch.tensor([tokenizer_src.token_to_id('[SOS]')], dtype=torch.int64), 
-            torch.tensor(source.ids, dtype=torch.int64),
-            torch.tensor([tokenizer_src.token_to_id('[EOS]')], dtype=torch.int64),
-            torch.tensor([tokenizer_src.token_to_id('[PAD]')] * (seq_len - len(source.ids) - 2), dtype=torch.int64)
-        ], dim=0).to(device)
-        source_mask = (source != tokenizer_src.token_to_id('[PAD]')).unsqueeze(0).unsqueeze(0).int().to(device)
-        encoder_output = model.encode(source, source_mask)
+    
+    tokenizer_path = Path(config['tokenizer_file'])
+    tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    tokenized_text = tokenizer.encode(text)
 
-        # Initialize the decoder input with the sos token
-        decoder_input = torch.empty(1, 1).fill_(tokenizer_trgt.token_to_id('[SOS]')).type_as(source).to(device)
+    sos_token = torch.tensor([tokenizer.token_to_id("[SOS]")], dtype=torch.int64).to(device)
+    decoder_input = torch.cat( 
+            [
+                sos_token,
+                torch.tensor(tokenized_text, dtype=torch.int64),
+            ],
+            dim=0,
+        )
+    
+    final_output = ""
+    # Generate output word by word
+    while decoder_input.size(1) < config['seq_len']:
 
-        # Print the source sentence and target start prompt
-        if label != "": print(f"{f'ID: ':>12}{id}") 
-        print(f"{f'SOURCE: ':>12}{sentence}")
-        if label != "": print(f"{f'TARGET: ':>12}{label}") 
-        print(f"{f'PREDICTED: ':>12}", end='')
-        
-        # Generate the translation word by word
-        while decoder_input.size(1) < seq_len:
-            # Build mask for target and calculate output
-            decoder_mask = torch.triu(torch.ones((1, decoder_input.size(1), decoder_input.size(1))), diagonal=1).type(torch.int).type_as(source_mask).to(device)
-            out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
+        decoder_output = model.decode(decoder_input)
 
-            # Project next token
-            prob = model.project(out[:, -1])
-            _, next_word = torch.max(prob, dim=1)
-            decoder_input = torch.cat([decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)], dim=1)
+        probs = model.project(decoder_output[:, -1]) # get probabilities for the next token
+        _, next_token = torch.max(probs, dim=1) # select one with the highest probability
 
-            # Build the translated word
-            translated_word = tokenizer_trgt.decode([next_word.item()])
+        decoder_input = torch.cat([decoder_input, torch.empty(1, 1).long().fill_(next_token.item()).to(device)], dim=1)
 
-            # Append the translated word to the result string
-            translation_result += translated_word + ' '
+        next_word = tokenizer.decode([next_token.item()])
 
-            # Break if we predict the end of sentence token
-            if next_word == tokenizer_trgt.token_to_id('[EOS]'):
-                break
+        final_output += next_word + ' '
 
-    # Convert ids to tokens
-    return translation_result.strip()
+        # Break if <EOS> was predicted
+        if next_word == tokenizer.token_to_id('[EOS]'):
+            break
+
+    return final_output.strip()
