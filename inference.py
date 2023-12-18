20314ef3186429bbb0c963b38f914bb58c436285
Oleksandr <akm.oleksandr@gmail.com>
2023-12-18 00:20:38 -0600

    top_k and temp

--- inference.py
+++ inference.py
@@ -3,9 +3,10 @@ from config import latest_weights_file_path
 from model import build_transformer
 from tokenizers import Tokenizer
 import torch
+import torch.nn.functional as F
 
 @torch.no_grad()
-def get_response(config, text):
+def get_response(config, text, temperature, top_k):
     # Define the device, tokenizers, and model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     tokenizer = Tokenizer.from_file(str(Path(config['tokenizer_file'])))
@@ -38,7 +39,8 @@ def get_response(config, text):
         decoder_output = model.decode(decoder_input)
 
         probs = model.project(decoder_output[:, -1]) # get probabilities for the next token
-        _, next_token = torch.max(probs, dim=1) # select one with the highest probability
+        
+        next_token = _get_next_token(probs, temperature, top_k)
 
         decoder_input = torch.cat([decoder_input, torch.empty(1, 1).long().fill_(next_token.item()).to(device)], dim=1)
 
@@ -51,3 +53,20 @@ def get_response(config, text):
         final_output += next_word + ' '
 
     return final_output.strip()
+
+def _get_next_token(probs, temperature=0.8, top_k=None):
+    if temperature == 0:
+        _, next_token = torch.max(probs, dim=1)  # select token with the highest probability
+        return next_token
+    elif temperature > 0:
+        scaled_probs = F.softmax(probs / temperature, dim=1)
+        
+        if top_k is not None:
+            # Apply top-k filtering
+            values, indices = torch.topk(scaled_probs, top_k, dim=1)
+            scaled_probs = torch.zeros_like(scaled_probs).scatter(1, indices, values)
+        
+        next_token = torch.multinomial(scaled_probs, 1).item()
+        return next_token
+    else:
+        raise ValueError("Temperature must be a positive value.")
